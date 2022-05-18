08ec4ff65f6962df0294c3faa688e4a2bc004b2a
ali0927 <kevinali0927@gmail.com>
2022-05-18 01:53:36 -0700

    init dev

--- App.js
+++ App.js
@@ -72,9 +72,6 @@ const App = () => {
           <Section title="Debug">
             <DebugInstructions />
           </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
           <LearnMoreLinks />
         </View>
       </ScrollView>
