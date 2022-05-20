6d33ce6af71664f80d390899889c562991fe5fa3
ali0927 <kevinali0927@gmail.com>
2022-05-20 13:06:45 -0700

    add created section

--- screens/components/authorPage/profile.js
+++ screens/components/authorPage/profile.js
@@ -1,11 +1,146 @@
-import React from "react";
-import {View, Text} from "react-native";
-import { ProfileDetails } from "./profileDetails";
-export const Profile = () => {
-    return(
+import React, {useState} from 'react';
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
+
+export const Profile = ({navigation}) => {
+  const [userEmail, setUserEmail] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [isDetails, setIsDetails] = useState(true);
+
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          width: '100%',
+          padding: 20,
+        }}>
+        <Text style={styles.text3}>{isDetails ? "Profile Details" : "Change Password"}</Text>
+        <TouchableOpacity onPress={() => setIsDetails(!isDetails)}>
+          <Text style={styles.text2}>{isDetails ? "Change Password " : "Profile Details "}&#10148;</Text>
+        </TouchableOpacity>
+      </View>
+      {isDetails && (
         <View>
-            {/* <Text style={{color: "#fff", fontSize: 25}}>Profile Section</Text> */}
-            <ProfileDetails />
+          <Text style={styles.text1}>Name</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Name"
+            autoCapitalize="none"
+            placeholderTextColor="white"
+            onChangeText={val => setUserEmail(val)}
+          />
+          <Text style={styles.text1}>Email</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            autoCapitalize="none"
+            placeholderTextColor="white"
+            onChangeText={val => setUserEmail(val)}
+          />
+          <Text style={styles.text1}>Mobile Number</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Mobile Number"
+            autoCapitalize="none"
+            placeholderTextColor="white"
+            onChangeText={val => setUserEmail(val)}
+          />
+          <Text style={styles.text1}>Background</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Background"
+            autoCapitalize="none"
+            placeholderTextColor="white"
+            onChangeText={val => setUserEmail(val)}
+          />
+          <TouchableOpacity style={styles.button} onPress={() => signIn()}>
+            <Text style={styles.text3}>Save</Text>
+          </TouchableOpacity>
         </View>
-    )
-}
+      )}
+      {!isDetails&& <View>
+          <Text style={styles.text1}>Old Password</Text>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            onChangeText={val => setUserEmail(val)}
+          />
+          <Text style={styles.text1}>New Password</Text>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            onChangeText={val => setUserEmail(val)}
+          />
+          <Text style={styles.text1}>Confirm New Password</Text>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            onChangeText={val => setUserEmail(val)}
+          />
+          
+          <TouchableOpacity style={styles.button} onPress={() => signIn()}>
+            <Text style={styles.text3}>Change</Text>
+          </TouchableOpacity>
+        </View>}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#14142f',
+  },
+  input: {
+    width: 350,
+    height: 55,
+    backgroundColor: '#534f77',
+    margin: 10,
+    padding: 8,
+    paddingLeft: 20,
+    color: 'white',
+    borderRadius: 14,
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  text1: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '500',
+    textAlign: 'left',
+    width: 350,
+    marginLeft: 20,
+    marginTop: 15,
+  },
+  text2: {
+    color: '#6164ff',
+    fontSize: 16,
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+  text3: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: '700',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+  },
+  button: {
+    marginTop: 30,
+    paddingTop: 10,
+    backgroundColor: '#6164ff',
+    borderRadius: 12,
+    width: 350,
+    margin: 10,
+    height: 50,
+  },
+});
--- screens/pages/explorer.js
+++ screens/pages/explorer.js
@@ -3,7 +3,7 @@ import {View, Text, Button} from "react-native";
 export const ExplorerScreen = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Explorer screen</Text>
+      <Text style={{fontSize: 20}}>Explorer screen</Text>
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
