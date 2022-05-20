a40889464efb609011685aed897d98d02f15d939
ali0927 <kevinali0927@gmail.com>
2022-05-20 02:29:39 -0700

    add image picker

--- screens/components/authorPage/overview.js
+++ screens/components/authorPage/overview.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -6,41 +6,93 @@ import {
   TextInput,
   Image,
   Button,
+  Platform,
   TouchableOpacity,
 } from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
 import imgAvatar from '../../../assets/img/avatars/avatar.jpg';
 import badgeMark from '../../../assets/img/avatars/avatar.jpg';
-import socialImg1 from '../../../assets/img/avatars/avatar.jpg';
-import socialImg2 from '../../../assets/img/avatars/avatar.jpg';
-import socialImg3 from '../../../assets/img/avatars/avatar.jpg';
-import socialImg4 from '../../../assets/img/avatars/avatar.jpg';
+import socialImg1 from '../../../assets/img/icons/social1.png';
+import socialImg2 from '../../../assets/img/icons/social2.png';
+import socialImg3 from '../../../assets/img/icons/social3.png';
+import socialImg4 from '../../../assets/img/icons/social4.png';
+
+const SERVER_URL = 'http://localhost:3000';
+
+const createFormData = (photo, body = {}) => {
+  const data = new FormData();
+
+  data.append('photo', {
+    name: photo.fileName,
+    type: photo.type,
+    uri: Platform.OS === 'ios' ? photo.uri.replace('file://', '') : photo.uri,
+  });
+
+  Object.keys(body).forEach(key => {
+    data.append(key, body[key]);
+  });
+
+  return data;
+};
 
 export const Overview = () => {
+  const [photo, setPhoto] = useState(null);
+
+  const handleChoosePhoto = async () => {
+    const result = await launchImageLibrary({mediaType: 'photo'}, response => {
+      console.log('ResponseImage', response);
+      if (response) {
+        setPhoto(response.assets[0]);
+      }
+    });
+  };
+
+  const handleUploadPhoto = () => {
+    fetch(`${SERVER_URL}/api/upload`, {
+      method: 'POST',
+      body: createFormData(photo, {userId: '123'}),
+    })
+      .then(response => response.json())
+      .then(response => {
+        console.log('response', response);
+      })
+      .catch(error => {
+        console.log('Error', error);
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.avatarDiv}>
-        <Image source={imgAvatar} />
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <TouchableOpacity onPress={handleChoosePhoto} style={styles.avatarDiv}>
+          <Image
+            source={{uri: photo ? photo.uri : imgAvatar}}
+            style={styles.avatarImg}
+          />
+        </TouchableOpacity>
         <Image source={badgeMark} />
+        {/* <Button title="Save" onPress={handleUploadPhoto} /> */}
       </View>
+        
       <Text style={styles.text1}>Mislan</Text>
       <Text style={styles.text2}>
         Please insert below your Binance Smart Chain wallet address
       </Text>
       <TextInput
         style={styles.input}
-        editable = {false}
+        editable={false}
         value="XAVUW3sw3ZunitokcLtemEfX3tGuX2plateWdh"
       />
       <TextInput
         style={styles.input}
-        editable = {false}
+        editable={false}
         value="https://bksbackstage.io"
       />
       <View style={styles.socialDiv}>
-        <Image source={socialImg1} />
-        <Image source={socialImg2} />
-        <Image source={socialImg3} />
-        <Image source={socialImg4} />
+        <Image source={socialImg1} style={styles.socialImg} />
+        <Image source={socialImg2} style={styles.socialImg} />
+        <Image source={socialImg3} style={styles.socialImg} />
+        <Image source={socialImg4} style={styles.socialImg} />
       </View>
       <View style={styles.divider}></View>
       <View style={styles.followDiv}>
@@ -50,7 +102,7 @@ export const Overview = () => {
         </View>
         <TouchableOpacity
           style={styles.button}
-          onPress={() => console.log("FollowButton Clicked")}>
+          onPress={() => console.log('FollowButton Clicked')}>
           <Text style={styles.text3}>Follow</Text>
         </TouchableOpacity>
       </View>
@@ -61,7 +113,20 @@ export const Overview = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20
+    padding: 20,
+  },
+  avatarDiv: {
+    width: 250,
+    height: 250,
+    borderRadius: 20,
+    borderColor: '#fff',
+    borderWidth: 3,
+    marginTop: -150
+  },
+  avatarImg: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 20
   },
   input: {
     height: 55,
@@ -95,20 +160,30 @@ const styles = StyleSheet.create({
     textTransform: 'uppercase',
   },
   button: {
-      alignItems: "center",
-      justifyContent: "center",
-      height: 52,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 52,
     backgroundColor: '#6164ff',
     borderRadius: 12,
   },
   socialDiv: {
     flexDirection: 'row',
     width: '100%',
-    justifyContent: 'space-between',
+    marginTop: 30,
+  },
+  socialImg: {
+    marginRight: 20,
   },
   followDiv: {
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "space-between"
-  }
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  divider: {
+    width: '100%',
+    backgroundColor: '#887bff',
+    height: 2,
+    marginTop: 20,
+    marginBottom: 20,
+  },
 });
