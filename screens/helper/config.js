790034ddb26ec5c4143b1ab2b34fa100e65ecf60
ali0927 <kevinali0927@gmail.com>
2022-06-04 08:04:59 -0700

    change api base url to live from local

--- screens/helper/config.js
+++ screens/helper/config.js
@@ -1,8 +1,8 @@
 const config = {
-    API_BASE_URL: 'http://192.168.106.26:3000',
+    // API_BASE_URL: 'http://192.168.106.26:3000',
     // SITE_URL: 'http://192.168.106.26:3000',
     
-    // API_BASE_URL: 'http://ec2-3-143-0-249.us-east-2.compute.amazonaws.com:3000',
+    API_BASE_URL: 'http://ec2-3-143-0-249.us-east-2.compute.amazonaws.com:3000',
     // SITE_URL: 'http://ec2-3-143-0-249.us-east-2.compute.amazonaws.com',
 
     // API_BASE_URL: 'http://ec2-3-141-18-184.us-east-2.compute.amazonaws.com:3000',
