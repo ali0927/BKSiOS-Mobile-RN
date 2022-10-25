6264317aa9943fe6a8ab607146120234d224560f
ali0927 <kevinali0927@gmail.com>
2022-10-25 12:21:54 -0400

    update latest digital arts list

--- screens/components/homePage/eventsArtsCarousel.js
+++ screens/components/homePage/eventsArtsCarousel.js
@@ -15,7 +15,7 @@ import {useSelector} from 'react-redux';
 import likeBlueImg from '../../../assets/img/icons/like-fill.png';
 import likeImg from '../../../assets/img/icons/liked-white.png';
 import config from '../../helper/config';
-import {getLatestEventCards, updateEventLike} from '../../helper/event';
+import {getAllEventCards, updateEventLike} from '../../helper/event';
 import {getLikesNumber} from '../../utils';
 import Currency from '../currency/Currency';
 import CurrencySymbol from '../currency/CurrencySymbol';
@@ -153,12 +153,15 @@ const EventsArtsCarousel = () => {
   };
 
   useEffect(() => {
-    getLatestEventCards().then(res => {
+    getAllEventCards().then(res => {
       if (res.success) {
+        let temp = [];
+        temp = res.eventcards.sort(
+          (a, b) =>
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+        );
         setLatestEvents(
-          res.eventcards.filter(
-            eventcard => eventcard.category === 'Category2',
-          ),
+          temp.filter(eventcard => eventcard.category === 'Category2'),
         );
       }
     });
